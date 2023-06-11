01591e1e17154f2feadfa4116d6a8836fe2e6749
Ismael Andrade <maelandrade@yahoo.com.br>
2023-06-10 21:28:04 -0300

    feat: Added shadow and scroll event.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,40 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { MatSidenav } from '@angular/material/sidenav';
+import { fromEvent, map } from 'rxjs';
+
+
+export const SCROLL_CONTAINER = 'mat-sidenav-content';
+export const TEXT_LIMIT = 50;
+export const SHADOW_LIMIT = 100;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   public isSmallScreem = false;
-
+  public popText = false;
+  public applyShadow = false;
   constructor(private breakpointObserver: BreakpointObserver){ }
+
+  ngOnInit(): void {
+    const content = document.getElementsByClassName(SCROLL_CONTAINER)[0];
+
+    fromEvent(content, 'scroll')
+    .pipe(
+      map(() => content.scrollTop)
+    )
+    .subscribe({
+      next: (value: number) => this.determineHeader(value)
+    });
+  }
+
+  determineHeader(top: number){
+    this.popText = top >= TEXT_LIMIT;
+    this.applyShadow = top >= SHADOW_LIMIT;
+  }
 
   ngAfterContentInit(): void {
     this.breakpointObserver.observe(['(max-width: 800px)'])
