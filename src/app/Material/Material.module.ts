fd0fdf8da972df8808f9268d739e7cb3e0b79de6
Ismael Andrade <maelandrade@yahoo.com.br>
2023-06-14 22:16:13 -0300

    feat: Added form field input component.

--- src/app/Material/Material.module.ts
+++ src/app/Material/Material.module.ts
@@ -4,6 +4,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 
 @NgModule({
@@ -12,8 +14,16 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatInputModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline', floatLabel: 'always' }
+    }
+  ]
+
 })
 export class MaterialModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './Material/Material.module';
 import { ButtonsIconsComponent } from './buttons-icons/buttons-icons.component';
+import { FormFieldInputComponent } from './form-field-input/form-field-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonsIconsComponent
+    ButtonsIconsComponent,
+    FormFieldInputComponent
    ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule
